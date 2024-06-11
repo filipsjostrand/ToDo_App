d753dfb5c26932c0241c732ab91bee098bbc5ab3
filipsjostrand <filip_sjostrand@hotmail.com>
2024-06-11 21:07:00 +0200

    ADD script and css (added todo functionality)

---
+++ script.js
@@ -0,0 +1,17 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("You must write a todo text!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+    span.className = "delete-todo-cross"
+  }
+  inputBox.value = '';
+}
