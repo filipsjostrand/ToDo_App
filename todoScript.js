80795f491f4608a97ff699bfaba61457a29154c2
filipsjostrand <filip_sjostrand@hotmail.com>
2024-06-28 21:16:41 +0200

    ADD todoScript (limitedTodoSet)

--- todoScript.js
+++ todoScript.js
@@ -1,13 +1,27 @@
 let todoInput = document.getElementById("input-box-v2");
-let currentTodoSet = new Set();
+
+const maxElements = 3;
+let limitedTodoSet = new Set();
+// let currentTodoSet = new Set();
+
+function resetAllTodos() {
+  limitedTodoSet = new Set();
+  console.log("[...limitedTodoSet] = " + [...limitedTodoSet]);
+}
+
+// resetAllTodos();
+
+
 
 function addTaskToSet() {
-    if (todoInput.value === "") {
-        alert("You must write a todo text!");
-      }
-      else {
-        console.log("todoInput = " + todoInput);
-        currentTodoSet.add(todoInput.value);
-        console.log("[...currentTodoSet] = " + [...currentTodoSet]);
-      }
+  if (todoInput.value === "") {
+    alert("You must write a todo text!");
+  }
+  if (limitedTodoSet.size < maxElements) {
+    limitedTodoSet.add(todoInput.value);
+    // console.log(`${todoInput.value} added`);
+    console.log("[...limitedTodoSet] = " + [...limitedTodoSet]);
+  } else {
+    console.log("Current Todo limit (3) is reached.");
+  }
 }
