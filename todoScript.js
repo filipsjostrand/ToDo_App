effdcd799b4a4b9fd89d8117f7a78b01ae8fa597
filipsjostrand <filip_sjostrand@hotmail.com>
2024-07-03 05:35:02 +0200

    ADD todoScript.js (edit-funktionalitet, char-limit, FIX (reset todos)

--- todoScript.js
+++ todoScript.js
@@ -3,48 +3,148 @@ let todoOne = document.getElementById("todo-1");
 let todoTwo = document.getElementById("todo-2");
 let todoThree = document.getElementById("todo-3");
 let resetTodoButton = document.querySelector(".reset-todo-button");
-let newTodo = "";
+let newTodo;
 
-// 2024-07-01,Nästa gång:
-//  1. Fixa Edit-todos-funktionalitet (via rullist?)
-//  2. Spara todos till localStorage (!?)
+var dropDownSelection = document.getElementById("todo-select");
+var dropDownText;
+let editTodoOne;
 
-// let dropDownSelection = document.getElementById("todo-select");
+// 2024-07-03, Nästa gång:
+//  1. Skapa checkbox:ar
+//  2. Spara todos till localStorage (!?) ((Längst ner i addTaskToSet()(?)))
+//  3. Fixa CSS
+
+const characterCounter = document.getElementById('character-counter');
+const maximumChars = 50;
 
 const maxElements = 3;
 let limitedTodoSet = new Set();
 
+// _ _ _
+
+function checkNumberOfCharacters(todoInput) {
+
+  const currentLength = todoInput.value.length;
+
+  if (currentLength > maximumChars) {
+    todoInput.value = todoInput.value.slice(0, maximumChars); // Trim the excess characters
+    alert("Max character limit (50) exceeded!");
+  }
+
+  // Add or remove 'exceeded' class based on current length
+  if (currentLength > maximumChars) {
+    characterCounter.classList.add('exceeded');
+  } else {
+    characterCounter.classList.remove('exceeded');
+  }
+return todoInput.value;
+}
+
+function updateDropDownText() {
+  // Modify the variable inside the function
+    window.addEventListener('change', function() {
+    dropDownText = dropDownSelection.options[dropDownSelection.selectedIndex].text;
+  })
+}
+
+updateDropDownText();
+
 function resetTodoSet() {
   limitedTodoSet = new Set();
-  todoOne.innerHTML = "1. ..."
-  todoTwo.innerHTML = "2. ..."
-  todoThree.innerHTML = "3. ..."
+  todoOne.innerHTML = "1. ...";
+  todoTwo.innerHTML = "2. ...";
+  todoThree.innerHTML = "3. ...";
+  todoInput.value = "";
+  window.location.reload();
+}
+
+function editTodoSet() {
+  limitedTodoSet = new Set();
+  todoOne.innerHTML = ""
+  todoTwo.innerHTML = ""
+  todoThree.innerHTML = ""
+  limitedTodoSet.add(editTodoOne);
+  limitedTodoSet.add(editTodoTwo);
+  limitedTodoSet.add(editTodoThree);
+  todoOne.innerHTML = [...limitedTodoSet][0];
+  todoTwo.innerHTML = [...limitedTodoSet][1];
+  todoThree.innerHTML = [...limitedTodoSet][2];
+  todoInput.value = "";
 }
 
 function addTaskToSet() {
-  // console.log("addTaskToSet() körs")
+    // console.log("addTaskToSet() körs")
+    updateDropDownText();
+    checkNumberOfCharacters(todoInput);
+
   if (todoInput.value === "") {
     alert("You must write a todo text!");
   }
-  if (limitedTodoSet.size === maxElements) {
+
+   // Ändra specifika todos (edit)
+  if (limitedTodoSet.size === maxElements && dropDownText === "Todo list") {
     alert("Current Todo limit (3) is reached.");
   }
+  if (todoOne.innerHTML != "1. ..." && dropDownText !== "Todo list" && dropDownText === "#1" && todoInput.value !== "") {
+    editTodoOne = "1. " + todoInput.value
+    if (limitedTodoSet.has([...limitedTodoSet][1])) {
+      editTodoTwo = [...limitedTodoSet][1]}
+      else {
+        editTodoTwo = "...";
+      };
+      if (limitedTodoSet.has([...limitedTodoSet][2])) {
+        editTodoThree = [...limitedTodoSet][2]}
+        else {
+          editTodoThree = ". . .";
+        };
+    editTodoSet();
+  }
 
-  if (limitedTodoSet.size < maxElements) {
+  if (todoTwo.innerHTML != "2. ..." && dropDownText !== "Todo list" && dropDownText === "#2" && todoInput.value !== "") {
+    if (limitedTodoSet.has([...limitedTodoSet][0])) {
+      editTodoOne = [...limitedTodoSet][0]}
+      else {
+        editTodoOne = "... ";
+      };
+    editTodoTwo = "2. " + todoInput.value
+    if (limitedTodoSet.has([...limitedTodoSet][2])) {
+      editTodoThree = [...limitedTodoSet][2]}
+      else {
+        editTodoThree = " ...";
+      };
+    editTodoSet();
+  }
+  if (todoThree.innerHTML != "3. ..." && dropDownText !== "Todo list" && dropDownText === "#3" && todoInput.value !== "") {
+    console.log("r101 ooa)")
+    if (limitedTodoSet.has([...limitedTodoSet][0])) {
+      editTodoOne = [...limitedTodoSet][0]}
+      else {
+        editTodoOne = "... ";
+      };
+    if (limitedTodoSet.has([...limitedTodoSet][1])) {
+      editTodoTwo = [...limitedTodoSet][1]}
+      else {
+        editTodoTwo = "... ";
+      };
+    editTodoThree = "3. " + todoInput.value
+    editTodoSet();
+  }
 
-    if (todoOne.innerHTML === "1. ...") {
+  // Lägg till todos (en i taget)
+  if (limitedTodoSet.size < maxElements && todoInput.value !== "") {
+    if (todoOne.innerHTML === "1. ..." && dropDownText === "Todo list") {
       newTodo = "1. " + todoInput.value
       limitedTodoSet.add(newTodo);
       todoOne.innerHTML = [...limitedTodoSet][0];
       todoInput.value = "";
 
-    } else if (todoOne.innerHTML != "1. ..." && todoTwo.innerHTML === "2. ...") {
+    } else if (todoOne.innerHTML != "1. ..." && todoTwo.innerHTML === "2. ..." && dropDownText === "Todo list") {
       newTodo = "2. " + todoInput.value
       limitedTodoSet.add(newTodo);
       todoTwo.innerHTML = [...limitedTodoSet][1];
       todoInput.value = "";
 
-    } else if (todoOne.innerHTML != "1. ..." && todoTwo.innerHTML != "2. ..." && todoThree.innerHTML === "3. ...") {
+    } else if (todoOne.innerHTML != "1. ..." && todoTwo.innerHTML != "2. ..." && dropDownText === "Todo list" && todoThree.innerHTML === "3. ...") {
       newTodo = "3. " + todoInput.value
       limitedTodoSet.add(newTodo);
       todoThree.innerHTML = [...limitedTodoSet][2];
